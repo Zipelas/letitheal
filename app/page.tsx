e20d70145abbf380dd68563b268f4925331df046
Zipelas <mcshaffa@gmail.com>
2025-11-27 11:26:54 +0100

    added a section and style center

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 const page = () => {
   return (
-    <>
-      <h1 className='text-tangerine-calligraphy text-color font-bold '>
+    <section>
+      <h1 className='text-tangerine-calligraphy text-color font-bold text-center'>
         Let it Heal
       </h1>
       <h1>Let it Heal</h1>
       <h2>Let it Heal</h2>
       <h3>Let it Heal</h3>
       <p>Let it Heal</p>
-    </>
+    </section>
   );
 };
 
