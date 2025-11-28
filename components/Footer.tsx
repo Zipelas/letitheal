abd0bff5479537629bf6167843d4cb4f5a016330
Zipelas <mcshaffa@gmail.com>
2025-11-28 15:07:26 +0100

    created Footer with 3 divs containing logo, contactinfo & socials

---
+++ components/Footer.tsx
@@ -0,0 +1,86 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link';
+
+const Footer = () => {
+  return (
+    <footer className='flex justify-between flex-col sm:flex-row m-4'>
+      <div className='m-4 sm:m-2'>
+        <Link
+          href='/'
+          className='flex items-center gap-2'
+          aria-label='Let it Heal startsida'>
+          <Image
+            src='/logo.png'
+            alt='Let it Heal Logo'
+            width={32}
+            height={32}
+          />
+          <span className='text-tangerine-calligraphy font-bold text-2xl sm:text-3xl'>
+            Let it Heal
+          </span>
+        </Link>
+      </div>
+      <div className='m-4 sm:m-2'>
+        <p>Kontakt:</p>
+        <p>Skånegatan 17</p>
+        <p>412 52 Göteborg</p>
+        <p>031- 12 34 56</p>
+        <p>hello@letitheal.se</p>
+        <p>letitheal.se</p>
+      </div>
+      <nav>
+        <ul className='socials'>
+            {[1,2,3,4].map((icon) => (
+                <li key={icon}>Icon {icons}</li>
+            ))}
+        </ul>
+      </nav>
+      {/* <nav className='m-4 sm:m-2'>
+        <ul>
+          <li className='flex gap-2 py-2'>
+            <Image
+              src='/icons/socials/facebook.svg'
+              alt='Facebook Logo'
+              width={32}
+              height={32}
+            />
+            <p>Facebook</p>
+          </li>
+          <li className='flex gap-2 py-2'>
+            <Image
+              src='/icons/socials/instagram.svg'
+              alt='Instagram Logo'
+              width={32}
+              height={32}
+            />
+            <p>Instagram</p>
+          </li>
+          <li className='flex gap-2 py-2'>
+            <Image
+              src='/icons/socials/x.svg'
+              alt='X Logo'
+              width={32}
+              height={32}
+            />
+            <p>X</p>
+          </li>{' '}
+          <li className='flex gap-2 py-2'>
+            <Image
+              src='/icons/socials/youtube.svg'
+              alt='Youtube Logo'
+              width={32}
+              height={32}
+            />
+            <p>Youtube</p>
+          </li>
+          <li>
+            <p>&copy; 2025 Let it Heal</p>
+          </li>
+        </ul>
+      </nav> */}
+    </footer>
+  );
+}
+
+export default Footer
