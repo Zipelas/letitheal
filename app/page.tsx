0627249d89184d15c9543a3e35c70ceaf358c4fc
Zipelas <mcshaffa@gmail.com>
2025-11-28 10:47:13 +0100

    added Image and a <p>

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
+import Image from 'next/image';
 const page = () => {
   return (
     <section>
       <h1 className='text-tangerine-calligraphy text-color font-bold text-center'>
         Let it Heal
       </h1>
-      <h1>Let it Heal</h1>
-      <h2>Let it Heal</h2>
-      <h3>Let it Heal</h3>
-      <p>Let it Heal</p>
+      <Image
+        src='/hero-1.png'
+        alt='Hero image'
+        width={600}
+        height={1400}
+        className='hero-1 mx-auto my-4 rounded-lg shadow-lg'
+      />
+      <p className='text-center text-color m-4 px-4'>
+        Välkommen till Let it Heal! Här kan du hitta information om healing, med
+        fokus på Reiki, en holistisk japansk behandlingsmetod som syftar till
+        stressreducering, avslappning och andlig läkning.
+      </p>
     </section>
   );
 };
