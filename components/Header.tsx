223e350505d75c9adf70dda971c0b58b0c5a193e
Zipelas <mcshaffa@gmail.com>
2025-11-28 10:48:06 +0100

    created a Header component

---
+++ components/Header.tsx
@@ -0,0 +1,50 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Header = () => {
+  return (
+    <header className='flex flex-row items-center justify-between p-4 shadow-sm'>
+      <div className='flex items-center gap-3'>
+        <Link href='/'>
+          <Image
+            src='/logo.png'
+            alt='Let it Heal Logo'
+            width={32}
+            height={32}
+          />
+        </Link>
+        <Link
+          href='/'
+          className='text-tangerine-calligraphy font-bold text-2xl sm:text-3xl'>
+          Let it Heal
+        </Link>
+      </div>
+      <nav>
+        <ul>
+          <Link
+            href='/#onSite'
+            className='text-inter-sans-serif p-4'>
+            På plats
+          </Link>
+          <Link
+            href='/#online'
+            className='text-inter-sans-serif p-4'>
+            Online
+          </Link>
+          <Link
+            href='/booking'
+            className='text-inter-sans-serif p-4'>
+            Boka
+          </Link>
+          <Link
+            href='/reviews'
+            className='text-inter-sans-serif p-4'>
+            Recensioner
+          </Link>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
