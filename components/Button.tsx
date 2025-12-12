c6680556c79ae58b52e7f8ebe76b3b619610d563
Zipelas <mcshaffa@gmail.com>
2025-12-12 16:32:49 +0100

    added props

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,13 +1,27 @@
 'use client';
 
-const Button = () => {
+import React from 'react';
+
+type ButtonProps = {
+  className?: string;
+  children?: React.ReactNode;
+  onClick?: () => void;
+  type?: 'button' | 'submit' | 'reset';
+};
+
+const Button: React.FC<ButtonProps> = ({
+  className = '',
+  children = 'Login',
+  onClick,
+  type = 'button',
+}) => {
   return (
     <button
-      type='button'
+      type={type}
       id='login-button'
-      className='mx-auto'
-      onClick={() => console.log('CLICK')}>
-      Login
+      className={`mx-auto ${className}`}
+      onClick={onClick ?? (() => console.log('CLICK'))}>
+      {children}
     </button>
   );
 };
