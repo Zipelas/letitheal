241cd9833303ffd9d24efea354297120d67d5214
Zipelas <mcshaffa@gmail.com>
2025-12-14 17:15:19 +0100

    created route.for auth

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,34 @@
+import { dbConnect } from '@/lib/mongoose';
+import User from '@/models/User';
+import argon2 from 'argon2';
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+
+export const authOptions: NextAuthOptions = {
+  session: { strategy: 'jwt' },
+  providers: [
+    Credentials({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) return null;
+        await dbConnect();
+        const user = await User.findOne({ email: credentials.email }).lean();
+        if (!user || !user.passwordHash) return null;
+        const ok = await argon2.verify(user.passwordHash, credentials.password);
+        if (!ok) return null;
+        return { id: String(user._id), name: user.name, email: user.email };
+      },
+    }),
+  ],
+  pages: {
+    signIn: '/login',
+  },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
